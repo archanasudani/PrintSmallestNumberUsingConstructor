d696290a3189258eeac0fe0a529001aa5464aaf8
archana sudani <archanasudani@gmail.com>
2019-01-17 22:21:37 +0000

    My First Commit

---
+++ src/loveProgramming/Main.java
@@ -0,0 +1,11 @@
+package loveProgramming;
+public class Main {
+           public static void main(String[] args) {
+            SmallNumber a = new SmallNumber(new int[]{16,24,46,67,79,}) ;
+            SmallNumber b = new SmallNumber(new int[]{46,65,06,96,86,25});
+            SmallNumber c = new SmallNumber(new int[]{2072,2028,2013,2045,2032});
+            SmallNumber d = new SmallNumber(new int[]{19,05,13,49,59,99,75,64,});
+            SmallNumber e = new SmallNumber(new int[]{8,65,78});
+            SmallNumber f = new SmallNumber(new int[]{1996,1988,1989});
+           }
+}
